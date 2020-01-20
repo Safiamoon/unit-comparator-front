806a47492aa52a165692c2d5835fabc57770e077
Gwendoline MATHAIS <gma@cnam-lyon.peda>
2020-01-20 15:39:21 +0100

    #1: Mow69/unit-comparator-php#1 api connection - done

--- src/components/layouts/ComparatorForm.jsx
+++ src/components/layouts/ComparatorForm.jsx
@@ -1,64 +1,82 @@
 import React from 'react';
 import {useForm} from 'react-hook-form';
 import Axios from "axios";
-import {apiUrl} from "../../constants";
+import {apiUrl} from "../../constants"
 import {connect} from "../../Utils";
 
 const ComparatorForm = ({handleResultData}) => {
-    const {register, handleSubmit, setError} = useForm();
+        const {register, handleSubmit, setError} = useForm();
 
-    const onSubmit = data => {
-        console.log("données :", data);
+        const onSubmit = async (data) => {
+            console.log("données :", data);
 
-        // Axios.post(apiUrl, data)
-        //     .then(function (response) {
-        //         console.log("response", response);
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
-        //
+            // Axios.post(apiUrl, data)
+            //     .then(function (response) {
+            //         console.log("response", response);
+            //     })
+            //     .catch(function (error) {
+            //         console.log(error);
+            //     });
+            //
+            try {
+                const response = await connect(data);
+                const jsonResponse = await response.json();
 
-        connect(data)
-            .then(response => {
-                if (response.status < 200 || response.status >= 300)
-                   console.log("mlkdfmlsdkmksd");
-
-                handleResultData = result => {
-                    result = response.json();
-                    return result;
-                };
-            })
-            .catch(e => {
+                console.log("jsonResponse.result", jsonResponse.result);
+                handleResultData(jsonResponse.result);
+            } catch (e) {
                 setError("apiServer", "connection", "Une erreur est survenue");
-            });
+            }
+        };
 
-    };
+        return (
+            <>
+                <h1 className="mb-3">Convertisseur</h1>
 
-    return (
-        <>
-            <h1 className="mb-3">Convertisseur</h1>
+                <div id="form-content">
+                    <form className="form-inline" onSubmit={handleSubmit(onSubmit)}>
 
-            <div id="form-content">
-                <form className="form-inline" onSubmit={handleSubmit(onSubmit)}>
+                        <button type="submit" className="btn btn-primary">Convertir</button>
 
-                    <button className="btn btn-primary">Convertir</button>
+                        <input
+                            type="number"
+                            ref={register}
+                            name="valueToConvert"
+                            id="valueToConvert"
+                            className="form-control "
+                            placeholder="..."
+                        />
 
-                    <div className="form-group">
-                        <input type="number" ref={register} name="squaremeter" id="squaremeter"
-                               className="form-control "
-                               placeholder="..."/>
-                    </div>
-                    <div className="form-group">
-                        <p>m² en hectare</p>
-                    </div>
-                </form>
+                        <div className="form-group">
+                            <div className="form-group">
+                                <select defaultValue="Unité d'origine" className="custom-select" ref={register}
+                                        name="inUnit" id="inUnit">
+                                    <option value="m2">m²</option>
+                                    <option value="hectare">hectare</option>
+                                    <option value="kW">kWh</option>
+                                    <option value="kgCo2">kgCo2</option>
+                                </select>
+                            </div>
 
-            </div>
+                            <div className="mr-2 ml-2"><p>En</p></div>
+
+                            <div className="form-group">
+                                <select defaultValue="Unité de sortie" className="custom-select" ref={register}
+                                        name="outUnit" id="outUnit">
+                                    <option value="hectare">hectare</option>
+                                    <option value="m2">m²</option>
+                                    <option value="kW">kWh</option>
+                                    <option value="kgCo2">kgCo2</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
 
-        </>
+            </>
 
-    );
-};
+        );
+    }
+;
 export default ComparatorForm;
